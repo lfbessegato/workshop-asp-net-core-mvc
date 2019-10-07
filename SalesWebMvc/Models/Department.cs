e4f918f3690c02eb85d3bdca9593fbdf9f2f8ed2
Luciano Bessegato <lfrbessegato@gmail.com>
2019-10-07 14:44:26 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,35 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //Instanciado
+
+        //Construtores
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //Métodos
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        //Atributos Básicos
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus status { get; set; }
+        public Seller Seller { get; set; }
+
+        //Construtores
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            this.status = status;
+            Seller = seller;
+        }
+
+
+    }
+}
